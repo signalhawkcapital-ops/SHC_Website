2003db84828d005bfbcacc153be5aff9b9fce07c
signalhawkcapital-ops <signal.hawk.capital@gmail.com>
2025-09-15 20:18:22 -0400

    Add files via upload

--- website/website.py
+++ website/website.py
@@ -1,13 +1,12 @@
 from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route('/')
 def home():
-    # A super-simple page render; all copy lives in the template.
-    return render_template("index.html")
+    return render_template('index.html')
 
-# Render.com will look for "gunicorn website:app" by default if you set it that way
-# You can still run locally with:  python website.py
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
